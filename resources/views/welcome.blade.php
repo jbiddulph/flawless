809c0a7018b762dd450b0342ff5f3ceb4f41c635
John Biddulph <john.mbiddulph@gmail.com>
2020-03-21 18:35:46 +0000

    styling contact number

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -547,9 +547,8 @@
 
         <!-- Contact Section Form -->
         <div class="row">
-            <div class="col-lg-8 mx-auto">
-                <h3>07917 33 82 01</h3>
-                <br />
+            <div class="col-lg-8 mx-auto text-center">
+                <h3 class="mt-5 mb-5">07917 33 82 01</h3>
                 <a class="btn btn-outline-light btn-social mx-1" href="https://www.facebook.com/Flawless-Medical-Aesthetics-274859666779513/">
                     <i class="fab fa-fw fa-facebook-f"></i>
                 </a>
