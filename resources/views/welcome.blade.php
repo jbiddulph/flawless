ea476928753928fc51b33ee42e0376c16dc34119
John Biddulph <john.mbiddulph@gmail.com>
2020-03-21 18:34:10 +0000

    removed contact form for now

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -548,42 +548,50 @@
         <!-- Contact Section Form -->
         <div class="row">
             <div class="col-lg-8 mx-auto">
+                <h3>07917 33 82 01</h3>
+                <br />
+                <a class="btn btn-outline-light btn-social mx-1" href="https://www.facebook.com/Flawless-Medical-Aesthetics-274859666779513/">
+                    <i class="fab fa-fw fa-facebook-f"></i>
+                </a>
+                <a class="btn btn-outline-light btn-social mx-1" href="https://www.instagram.com/flawlessmedaesthetics/">
+                    <i class="fab fa-instagram"></i>
+                </a>
                 <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19. -->
-                <form name="sentMessage" id="contactForm" novalidate="novalidate">
-                    <div class="control-group">
-                        <div class="form-group floating-label-form-group controls mb-0 pb-2">
-                            <label>Name</label>
-                            <input class="form-control" id="name" type="text" placeholder="Name" required="required" data-validation-required-message="Please enter your name.">
-                            <p class="help-block text-danger"></p>
-                        </div>
-                    </div>
-                    <div class="control-group">
-                        <div class="form-group floating-label-form-group controls mb-0 pb-2">
-                            <label>Email Address</label>
-                            <input class="form-control" id="email" type="email" placeholder="Email Address" required="required" data-validation-required-message="Please enter your email address.">
-                            <p class="help-block text-danger"></p>
-                        </div>
-                    </div>
-                    <div class="control-group">
-                        <div class="form-group floating-label-form-group controls mb-0 pb-2">
-                            <label>Phone Number</label>
-                            <input class="form-control" id="phone" type="tel" placeholder="Phone Number" required="required" data-validation-required-message="Please enter your phone number.">
-                            <p class="help-block text-danger"></p>
-                        </div>
-                    </div>
-                    <div class="control-group">
-                        <div class="form-group floating-label-form-group controls mb-0 pb-2">
-                            <label>Message</label>
-                            <textarea class="form-control" id="message" rows="5" placeholder="Message" required="required" data-validation-required-message="Please enter a message."></textarea>
-                            <p class="help-block text-danger"></p>
-                        </div>
-                    </div>
-                    <br>
-                    <div id="success"></div>
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-primary btn-xl" id="sendMessageButton">Send</button>
-                    </div>
-                </form>
+{{--                <form name="sentMessage" id="contactForm" novalidate="novalidate">--}}
+{{--                    <div class="control-group">--}}
+{{--                        <div class="form-group floating-label-form-group controls mb-0 pb-2">--}}
+{{--                            <label>Name</label>--}}
+{{--                            <input class="form-control" id="name" type="text" placeholder="Name" required="required" data-validation-required-message="Please enter your name.">--}}
+{{--                            <p class="help-block text-danger"></p>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                    <div class="control-group">--}}
+{{--                        <div class="form-group floating-label-form-group controls mb-0 pb-2">--}}
+{{--                            <label>Email Address</label>--}}
+{{--                            <input class="form-control" id="email" type="email" placeholder="Email Address" required="required" data-validation-required-message="Please enter your email address.">--}}
+{{--                            <p class="help-block text-danger"></p>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                    <div class="control-group">--}}
+{{--                        <div class="form-group floating-label-form-group controls mb-0 pb-2">--}}
+{{--                            <label>Phone Number</label>--}}
+{{--                            <input class="form-control" id="phone" type="tel" placeholder="Phone Number" required="required" data-validation-required-message="Please enter your phone number.">--}}
+{{--                            <p class="help-block text-danger"></p>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                    <div class="control-group">--}}
+{{--                        <div class="form-group floating-label-form-group controls mb-0 pb-2">--}}
+{{--                            <label>Message</label>--}}
+{{--                            <textarea class="form-control" id="message" rows="5" placeholder="Message" required="required" data-validation-required-message="Please enter a message."></textarea>--}}
+{{--                            <p class="help-block text-danger"></p>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                    <br>--}}
+{{--                    <div id="success"></div>--}}
+{{--                    <div class="form-group">--}}
+{{--                        <button type="submit" class="btn btn-primary btn-xl" id="sendMessageButton">Send</button>--}}
+{{--                    </div>--}}
+{{--                </form>--}}
             </div>
         </div>
     </div>
